81458193b121c18039e58849230db1abe5a29c6b
Dylan Torres Cruz <dylan@programmer.city>
2019-09-21 16:49:26 -0500

    Finish class

--- segundaC/routes/index.js
+++ segundaC/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router  = express.Router();
-
+const Book = require('../models/Book.js')
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/books',(req,res,next)=>{
+  Book.find().then(books=>{
+    console.log('Cuantos libros',books)
+    res.render('books',{books})
+  }).catch(err=>console.log('el erro en find es:',err))
+  
+})
+
+router.get('/books/:bookId', (req, res, next) => {
+  let {bookId} = req.params
+
+  Book.findById(bookId)
+  .then(theBook => {
+    console.log('El libro',theBook)
+    res.render('bookDetail',{theBook});
+  })
+  .catch(error => {
+    console.log('Error while retrieving book details: ', error);
+  })
+
 });
 
 
